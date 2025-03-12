28ad055e4f50732e631d3b267ab22c84ef5eeb76
Documenter.jl <documenter@juliadocs.github.io>
2025-03-12 18:18:17 +0000

    build based on 9a56851

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ACI318_25ReinforcementDetails","category":"page"},{"location":"#ACI318_25ReinforcementDetails","page":"Home","title":"ACI318_25ReinforcementDetails","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ACI318_25ReinforcementDetails.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ACI318_25ReinforcementDetails]","category":"page"}]
+}
